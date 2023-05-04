9b7f402a467e98abab5dd3b3dac0adc13d69e0bd
94applekoo <94applekoo@naver.com>
2023-05-04 17:46:33 +0900

    S08P31A606-179 feat: API uri 수정

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,15 +1,15 @@
 export const requests = {
-  base_url: 'http://k8a606.p.ssafy.io:8080',
+  base_url: 'ws://k8a606.p.ssafy.io:8080',
 
   // stomp 연결
   CONNECT: '/stomp',
 
-  // 채팅 전송
+  // 채팅
   CHAT({roomId}) {
-    return `/stomp/chat/send/${roomId}`;
+    return `/stomp/pub/chat/enter/${roomId}`;
   },
 
-  // 실시간 위치 전송
+  // 실시간 위치
   LOCATION({roomId}) {
     return `/stomp/gps/location/${roomId}`;
   },
