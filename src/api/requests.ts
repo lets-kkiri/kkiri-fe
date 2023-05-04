b3b380bee8263105bcb8e75a526e03cbce1c7302
94applekoo <94applekoo@naver.com>
2023-05-04 15:44:35 +0900

    S08P31A606-179 feat: stomp 연결 구현

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,13 +1,16 @@
 export const requests = {
-  base_url: 'http://k8a606.p.ssafy.io',
+  base_url: 'http://k8a606.p.ssafy.io:8080',
+
+  // stomp 연결
+  CONNECT: '/stomp',
 
   // 채팅 전송
-  SEND_CHAT({roomId}) {
+  CHAT({roomId}) {
     return `/stomp/chat/send/${roomId}`;
   },
 
   // 실시간 위치 전송
-  SEND_LOCATION({roomId}) {
+  LOCATION({roomId}) {
     return `/stomp/gps/location/${roomId}`;
   },
 };
