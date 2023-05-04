a2dff275dca7cfa3552a7b1dba06641d55c23795
hanbeulYou <hanbeul.you@gmail.com>
2023-05-04 16:57:27 +0900

    S08P31A606-230 feat: token 관리 및 firebase 세팅

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+// import {Alert} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -20,6 +23,15 @@ import CreateMoim from './src/pages/CreateMoim';
 import usePermissions from './src/hooks/usePermissions';
 import RealtimeLocation from './src/pages/RealtimeLocation';
 
+// redux
+import userSlice from './src/slices/user';
+import {useAppDispatch} from './src/store';
+
+// FCM 및 푸쉬 알림
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+
 export type LoggedInParamList = {
   Orders: undefined;
   Settings: undefined;
@@ -30,8 +42,139 @@ export type LoggedInParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
-  // const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   usePermissions();
+
+  // FCM 처리
+  // async function requestUserPermission() {
+  //   const authStatus = await messaging().requestPermission();
+  //   const enabled =
+  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  //   console.log('[Authorization Status]', authStatus);
+  //   if (enabled) {
+  //     await messaging()
+  //       .getToken()
+  //       .then(token => {
+  //         console.log('[FCM Token]', token);
+  //       })
+  //       .catch(err => {
+  //         console.log('[FCM Token Error]', err);
+  //       });
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   requestUserPermission();
+  //   // getFcmToken();
+  //   const unsubscribe = messaging().onMessage(async remoteMessage => {
+  //     Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+  //     console.log('[FCM Message]', remoteMessage);
+  //     console.log('[TEST]');
+  //   });
+  //   return unsubscribe;
+  // }, []);
+
+  const dispatch = useAppDispatch();
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+  PushNotification.configure({
+    // (optional) 토큰이 생성될 때 실행됨(토큰을 서버에 등록할 때 쓸 수 있음)
+    onRegister: function (token: any) {
+      console.log('TOKEN:', token);
+    },
+
+    // (required) 리모트 노티를 수신하거나, 열었거나 로컬 노티를 열었을 때 실행
+    onNotification: function (notification: any) {
+      console.log('NOTIFICATION:', notification);
+      if (notification.channelId === 'riders') {
+        // if (notification.message || notification.data.message) {
+        //   store.dispatch(
+        //     userSlice.actions.showPushPopup(
+        //       notification.message || notification.data.message,
+        //     ),
+        //   );
+        // }
+      }
+      // process the notification
+
+      // (required) 리모트 노티를 수신하거나, 열었거나 로컬 노티를 열었을 때 실행
+      notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+
+    // (optional) 등록한 액션을 누렀고 invokeApp이 false 상태일 때 실행됨, true면 onNotification이 실행됨 (Android)
+    onAction: function (notification: any) {
+      console.log('ACTION:', notification.action);
+      console.log('NOTIFICATION:', notification);
+
+      // process the action
+    },
+
+    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+    onRegistrationError: function (err: Error) {
+      console.error(err.message, err);
+    },
+
+    // IOS ONLY (optional): default: all - Permissions to register.
+    permissions: {
+      alert: true,
+      badge: true,
+      sound: true,
+    },
+
+    // Should the initial notification be popped automatically
+    // default: true
+    popInitialNotification: true,
+
+    /**
+     * (optional) default: true
+     * - Specified if permissions (ios) and token (android and ios) will requested or not,
+     * - if not, you must call PushNotificationsHandler.requestPermissions() later
+     * - if you are not using remote notification or do not have Firebase installed, use this:
+     *     requestPermissions: Platform.OS === 'ios'
+     */
+    requestPermissions: true,
+  });
+
+  // 여러 종류의 채널로 세분화
+  PushNotification.createChannel(
+    {
+      channelId: 'hurry', // (required)
+      channelName: '재촉 알림', // (required)
+      channelDescription: '재촉할 때 울리는 알림', // (optional) default: undefined.
+      soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+      importance: 4, // (optional) default: 4. Int value of the Android notification importance
+      vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+    },
+    (created: boolean) =>
+      console.log(`createChannel riders returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+  );
+
+  // 토큰 설정
+  useEffect(() => {
+    async function getToken() {
+      try {
+        if (!messaging().isDeviceRegisteredForRemoteMessages) {
+          await messaging().registerDeviceForRemoteMessages();
+        }
+        const token = await messaging().getToken();
+        console.log('phone token', token);
+        // dispatch(userSlice.actions.setPhoneToken(token));
+        // 임시 Config
+        const Config = {
+          API_URL: 'k8a606.p.ssafy.io',
+        };
+        return axios.post(`${Config.API_URL}/api/auth/firebase`, {token});
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (isLoggedIn === true) {
+      getToken();
+    }
+  }, [isLoggedIn]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
