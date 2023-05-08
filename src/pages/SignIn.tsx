c073dc34360a493e81230b8f38ca79d4bd518a65
hanbeulYou <hanbeul.you@gmail.com>
2023-05-08 10:28:09 +0900

    S08P31A606-144 feat: kakao login settings

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, View} from 'react-native';
+import {
+  KakaoOAuthToken,
+  KakaoProfile,
+  getProfile as getKakaoProfile,
+  login,
+  logout,
+  unlink,
+} from '@react-native-seoul/kakao-login';
 
 function SignIn() {
+  const signInWithKakao = async (): Promise<void> => {
+    const token: KakaoOAuthToken = await login();
+    setResult(JSON.stringify(token));
+  };
+
+  const signOutWithKakao = async (): Promise<void> => {
+    const message = await logout();
+    setResult(message);
+  };
+
+  const getKakaoProfile = async (): Promise<void> => {
+    const profile: KakaoProfile = await getProfile();
+    setResult(JSON.stringify(profile));
+  };
+
+  const unlinkKakao = async (): Promise<void> => {
+    const message = await unlink();
+    setResult(message);
+  };
+
   return (
     <View>
-      <Text>로그인 화면입니다</Text>
+      <Button title="로그인" onPress={signInWithKakao} />
     </View>
   );
 }
