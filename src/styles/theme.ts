e88ef47cc9f429b14db0e7326713362d40b53bb2
94applekoo <94applekoo@naver.com>
2023-05-08 13:13:01 +0900

    S08P31A606-234 feat: theme 파일 작성 중

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import {DefaultTheme} from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  color: {
+    main: #5968F2;
+    sub :#FF9270;
+    background: #FFFFFF;
+    text: 
+}
---
+++ src/types/styled.d.ts
@@ -0,0 +1,24 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefulatTheme {
+    color: {
+      main: string;
+      sub: string;
+      background: string;
+      text: string;
+      grey00: string;
+      grey10: string;
+      grey20: string;
+      grey30: string;
+      grey40: string;
+      grey50: string;
+      grey60: string;
+      grey70: string;
+      grey80: string;
+      grey90: string;
+      grey100: string;
+      black: string;
+      white: string;
+    }
+}
