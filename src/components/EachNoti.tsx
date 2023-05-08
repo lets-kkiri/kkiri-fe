f0423f087f64d672399f1783dab404cc1be47577
hanbeulYou <hanbeul.you@gmail.com>
2023-05-08 17:12:40 +0900

    S08P31A606-232 chore: 앞으로 필요한 내용 기록

--- src/components/EachNoti.tsx
+++ src/components/EachNoti.tsx
@@ -3,9 +3,9 @@ import {Text, View} from 'react-native';
 import {notiType} from '../slices/noti';
 
 function EachNoti({noti}: {noti: notiType}) {
-  // if (noti === undefined) {
-  //   return null;
-  // }
+  // 여기에 알림 버튼 클릭 가능하게
+  // 왼쪽으로 슬라이드시 삭제 버튼 보이기
+  // 삭제 버튼 클릭 시 해당 값 삭제 처리 및 API? (백과 논의 필요)
   return (
     <View>
       <Text>{noti?.title}</Text>
