629c3ecbc6b87f53bcb7dd7c00287a46bc53cda6
hanbeulYou <hanbeul.you@gmail.com>
2023-05-09 16:36:42 +0900

    S08P31A606-233 feat: 백그라운드 알림 저장 기능

--- App.tsx
+++ App.tsx
@@ -9,15 +9,10 @@ import {Provider} from 'react-redux';
 import AppInner from './AppInner';
 
 // FCM 및 푸쉬 알림
-import messaging from '@react-native-firebase/messaging';
 import {PersistGate} from 'redux-persist/integration/react';
 
 function App() {
   usePermissions();
-
-  messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Message handled in the background!', remoteMessage);
-  });
 
   return (
     <Provider store={store}>
