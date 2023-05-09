920da3909097b12fcdbc6d94a6d83fe5f42cfb9a
hanbeulYou <hanbeul.you@gmail.com>
2023-05-09 17:47:09 +0900

    S08P31A606-143 feat: SignIn page 구성

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, View} from 'react-native';
+import {Button, View, StyleSheet} from 'react-native';
 import {
   KakaoOAuthToken,
   KakaoProfile,
@@ -31,10 +31,37 @@ function SignIn() {
   };
 
   return (
-    <View>
-      <Button title="로그인" onPress={signInWithKakao} />
+    <View style={styles.container}>
+      <Button
+        title="카카오 로그인"
+        onPress={() => {
+          signInWithKakao();
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingBottom: 100,
+  },
+  button: {
+    backgroundColor: '#FEE500',
+    borderRadius: 40,
+    borderWidth: 1,
+    width: 250,
+    height: 40,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 10,
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
 
 export default SignIn;
