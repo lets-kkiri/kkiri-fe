6c2a3a7d86a3c93252665ea13bab36f4418ec613
yjp8842 <yjp8842@naver.com>
2023-05-12 02:31:21 +0900

    S08P31A606-99 fix: socket 관련 로직 수정 중

--- src/slices/socket.ts
+++ src/slices/socket.ts
@@ -1,29 +1,47 @@
-import {createSlice} from '@reduxjs/toolkit';
-import {io, Socket} from 'socket.io-client';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 
-interface SocketState {
-  moimId: number;
-  socket: Socket;
-}
-
-const initialState: SocketState = {
+const initialState = {
   moimId: 0,
-  socket: io('/'),
+  socket: '',
 };
+
+export const socketConnect = createAsyncThunk(
+  'sockets/connect',
+  async (data: number, thunkAPI) => {
+    const response = await JSON.stringify(
+      new WebSocket(`wss://k8a606.p.ssafy.io/ws/api/${data}`),
+    );
+    return thunkAPI.fulfillWithValue(response);
+  },
+);
 
 const socketsSlice = createSlice({
   name: 'sockets',
   initialState,
   reducers: {
-    createSocket: (state, action) => {
-      const moimId = action.payload.moimId;
-      const newSocket = io(`wss://k8a606.p.ssafy.io/ws/api/${moimId}`);
-      return {...state, newSocket};
-    },
+    // createSocket: (state, action) => {
+    // const moimId = action.payload.moimId;
+    // const newSocket = JSON.stringify(
+    //   new WebSocket(`wss://k8a606.p.ssafy.io/ws/api/${moimId}`),
+    // );
+    // state.socket = newSocket;
+    // },
     // removeSocket: (state, action) => {
     //   const {id} = action.payload;
     //   delete state[id];
     // },
+  },
+  extraReducers: builder => {
+    builder.addCase(socketConnect.pending, state => {
+      console.log('pending');
+    });
+    builder.addCase(socketConnect.fulfilled, (state, action) => {
+      console.log('fulfilled');
+      state.socket = action.payload;
+    });
+    builder.addCase(socketConnect.rejected, (state, action) => {
+      console.log('reject', action.error);
+    });
   },
 });
 
