5f214ac5625a6672b8e7238f5baaaeb188817b77
hanbeulYou <hanbeul.you@gmail.com>
2023-05-15 16:59:08 +0900

    S08P31A606-296 feat: 로그인 토큰 로직 변경

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -24,38 +24,44 @@ const naverAPI = (url: string, options?: any) => {
   });
 };
 
-export const setTokenHeader = (config: any) => {
-  config.headers.Authorization =
-    'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNzgzMzc0NjQ4IiwiaXNzIjoiS0tJUkkiLCJleHAiOjE2OTYwMDU0NjMsImlhdCI6MTY4MzkwOTQ2M30.E6hPIi_78WVgRdbKsD5uIlVS8YQpj0eZC-QFzEkDFxv4MYJmHhOni9KdS77TGZNsQC5fJ8w_uEjCJNGVfLovSA';
-  // console.log(config);
-  return config;
-};
-
-// export const setTokenHeader = (config: any, token: string) => {
-//   config.headers.Authorization = `Bearer ${token}`;
+// export const setTokenHeader = (config: any) => {
+//   config.headers.Authorization =
+//     'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNzgzMzc0NjQ4IiwiaXNzIjoiS0tJUkkiLCJleHAiOjE2OTYwMDU0NjMsImlhdCI6MTY4MzkwOTQ2M30.E6hPIi_78WVgRdbKsD5uIlVS8YQpj0eZC-QFzEkDFxv4MYJmHhOni9KdS77TGZNsQC5fJ8w_uEjCJNGVfLovSA';
+//   // console.log(config);
 //   return config;
 // };
+
+export const setTokenHeader = (config: any, token: string) => {
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+};
 
 export const baseInstance = baseAPI(BASE_URL);
 export const authInstance = authAPI(BASE_URL);
 export const naverInstance = naverAPI(NAVER_URL);
 
-authInstance.interceptors.request.use(setTokenHeader);
+// authInstance.interceptors.request.use(setTokenHeader);
 
-// authInstance.interceptors.response.use(
-//   response => {
-//     return response;
-//   },
-//   async error => {
-//     const originalRequest = error.config;
+authInstance.interceptors.response.use(
+  response => {
+    console.log('리스폰스입니다', response);
+    return response;
+  },
+  async error => {
+    const originalRequest = error.config;
+    console.error('에러입니다', error);
+    if (
+      (error.response.status === 400 || error.response.status === 403) &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      const accessToken = await TokenRefreshService.refreshAccessToken();
+      console.log('새로 받은 엑세스 토큰', accessToken);
+      authInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+      console.log('디폴트 헤더:', axios.defaults.headers.common);
+      return authInstance(originalRequest);
+    }
 
-//     if (error.response.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
-//       const accessToken = await TokenRefreshService.refreshAccessToken();
-//       axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-//       return authInstance(originalRequest);
-//     }
-
-//     return Promise.reject(error);
-//   },
-// );
+    return Promise.reject(error);
+  },
+);
